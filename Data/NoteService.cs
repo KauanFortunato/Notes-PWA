b0dc79452f166f5598a0bd0b086a16523705da4c
KauanFortunato <kauanmatiasfortunato@gmail.com>
2024-06-14 18:31:46 +0100

    Criar e apagar notas, usando IdexedDB

---
+++ Data/NoteService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.JSInterop; // Permite chamadas de funções JavaScript a partir do código C# e vice-versa
+using System.Collections.Generic; // Manipulação e coleção de dados (listas, dicionários, conjuntos, etc.)
+using System.Threading.Tasks; // Tarefas Assíncronas
+
+namespace Notes.Data
+{
+    public class NoteService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public NoteService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task AddNoteAsync(Note note)
+        {
+            await _jsRuntime.InvokeVoidAsync("indexedDBFunctions.addNote", note);
+        }
+
+        public async Task<List<Note>> GetAllNotesAsync()
+        {
+            return await _jsRuntime.InvokeAsync<List<Note>>("indexedDBFunctions.getAllNotes");
+        }
+
+        public async Task DeleteNoteAsync(int id)
+        {
+            await _jsRuntime.InvokeVoidAsync("indexedDBFunctions.deleteNote", id);
+        }
+    }
+}
---
+++ wwwroot/indexedDBFunctions.js
@@ -0,0 +1,130 @@
+﻿window.indexedDBFunctions = {
+    db: null,
+
+    openDb: function () {
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open('notes-data', 1);
+
+            request.onupgradeneeded = function (event) {
+                const db = event.target.result;
+                if (!db.objectStoreNames.contains('notes')) {
+                    db.createObjectStore('notes', { keyPath: 'id' });
+                }
+                if (!db.objectStoreNames.contains('data')) {
+                    db.createObjectStore('data', { keypath: 'key' });
+                }
+            };
+
+            request.onsuccess = function (event) {
+                indexedDBFunctions.db = event.target.result;
+                resolve();
+            };
+
+            request.onerror = function () {
+                reject('Erro ao abrir o banco de dados');
+            }
+        })
+    },
+
+    getLastId: function () {
+        return new Promise((resolve, reject) => {
+            const transaction = indexedDBFunctions.db.transaction(['data'], 'readonly');
+            const store = transaction.objectStore('data');
+            const request = store.get('lastId');
+
+            request.onsuccess = function (event) {
+                const result = event.target.result;
+                if (result) {
+                    //console.log('Último ID: ' + result);
+                    resolve(result);
+                } else {
+                    //console.log('Nenhum último ID encontrado, retornando 0');
+                    resolve(0);
+                }
+            };
+
+            request.onerror = function () {
+                reject('Erro ao pegar o ultimo id');
+            }
+        });
+    },
+
+    saveLastId: function (lastId) {
+        return new Promise((resolve, reject) => {
+            const transaction = indexedDBFunctions.db.transaction(['data'], 'readwrite');
+            const store = transaction.objectStore('data');
+
+            const request = store.put(lastId, 'lastId'); 
+
+            request.onsuccess = function () {
+                //console.log('Ultimo id salvo: ' + lastId);
+                resolve();
+            }
+
+            request.onerror = function () {
+                reject('Erro ao salvar o último id');
+            }
+        });
+    },
+
+    addNote: function (note) {
+        return new Promise(async (resolve, reject) => {
+            let lastId = await indexedDBFunctions.getLastId();
+            note.id = ++lastId;
+            console.log("Id da nota: " + note.id);
+
+            const transaction = indexedDBFunctions.db.transaction(['notes'], 'readwrite');
+            const store = transaction.objectStore('notes');
+            const request = store.add(note);
+
+            request.onsuccess = async function () {
+                await indexedDBFunctions.saveLastId(lastId);
+                resolve();
+            };
+
+            request.onerror = function () {
+                reject('Erro ao adicionar nota');
+            };
+        });
+    },
+
+    getAllNotes: function () {
+        return new Promise((resolve, reject) => {
+            const transaction = indexedDBFunctions.db.transaction(['notes'], 'readonly');
+            const store = transaction.objectStore('notes');
+            const request = store.getAll();
+
+            request.onsuccess = function (event) {
+                resolve(event.target.result);
+            };
+
+            request.onerror = function () {
+                reject('Erro ao ler notas');
+            };
+        });
+    },
+
+    deleteNote: function (id) {
+        return new Promise((resolve, reject) => {
+            const transaction = indexedDBFunctions.db.transaction(['notes'], 'readwrite');
+            const store = transaction.objectStore('notes');
+            const request = store.delete(id);
+
+            request.onsuccess = function () {
+                resolve();
+            };
+
+            request.onerror = function () {
+                reject('Erro ao ler notas');
+            };
+        });
+    }
+};
+
+indexedDBFunctions.openDb()
+    .then(() => {
+        console.log('Banco de dados IndexedDB aberto com sucesso!');
+    })
+    .catch(error => {
+        console.error('Erro ao abrir o banco de dados IndexedDB:', error);
+    });
